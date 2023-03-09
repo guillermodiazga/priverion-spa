a2590bfb0f769270f6b740e1c62ad9e6f4cb6350
Guillermo Díaz Gallego <guillermodiazga@gmail.com>
2023-03-09 15:13:42 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import AppRouter from './appRouter';
+import Navigation from './navigation';
+import Stack from 'react-bootstrap/Stack';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Stack gap={2} className="col-md-7 mx-auto">
+        <Navigation />
+        <br/>
+        <AppRouter />
+      </Stack>
+    </Provider>
   );
 }
 
